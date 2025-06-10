318d356a75609e2fe3c1256f2bd9415a05d8d7d9
Elvin <elvinsrkrov@gmail.com>
2025-06-10 22:29:52 +0400

    frontend dependencies && vite config updated

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,50 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{port:5173}
-})
+  server: { port: 5173 },
+  build: {
+    rollupOptions: {
+      treeshake: {
+        preset: "recommended",
+        moduleSideEffects: false,
+      },
+      output: {
+        manualChunks: {
+          vendor: ["react", "react-dom", "react-router-dom"],
+          forms: ["react-hook-form", "@hookform/resolvers", "yup"],
+          utils: [
+            "axios",
+            "jwt-decode",
+            "react-toastify",
+            "react-helmet-async",
+            "react-loading-skeleton",
+          ],
+        },
+      },
+    },
+    minify: 'esbuild',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+        pure_funcs: ["console.log", "console.info"],
+      },
+    },
+    sourcemap: false,
+    target: "esnext",
+  },
+  optimizeDeps: {
+    include: [
+      "react",
+      "react-dom",
+      "react-router-dom",
+      "axios",
+      "dayjs",
+      "react-hook-form",
+      "yup",
+    ],
+    exclude: ["@vite/client", "@vite/env"],
+  },
+});
