9977b0cfae691cb3164e1b3797bab72ea5715ef7
Elvin <elvinsrkrov@gmail.com>
2025-06-12 17:13:55 +0400

    cors config updated

--- backend/server.js
+++ backend/server.js
@@ -42,7 +42,10 @@ app.use(mongoSanitize());
 app.use(xss());
 app.use(
   cors({
-    origin: ["http://localhost:5174", "http://localhost:5173"],
+    origin: [
+      "https://medi-meet-web-six.vercel.app",
+      "https://medi-meet-admin.vercel.app",
+    ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
